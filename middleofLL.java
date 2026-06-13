8fcaa38783f49a94d81478205dc546265fb90125
https://github.com/Roshni-Singh27 <roshnisingh.27072005@gmail.com>
2026-06-13 11:56:19 +0530

    Done Middle of the LL

---
+++ middleofLL.java
@@ -0,0 +1,33 @@
+package DSA;
+class ListNode {
+    int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+public class middleofLL {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
+/*
+int cnt=0;
+         ListNode temp=head;
+    while(temp!=null){
+        temp=temp.next;
+        cnt++;
+    }
+    int mid=cnt/2;
+    ListNode move=head;
+    for(int i=0;i<mid;i++){
+        move=move.next;
+    }
+    return move;
+ */
