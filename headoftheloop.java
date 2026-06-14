158a56ab5e2965ac1ae7ad9abae67abb3c3f07a1
https://github.com/Roshni-Singh27 <roshnisingh.27072005@gmail.com>
2026-06-14 22:29:18 +0530

    Find the starting point of loop in LL

---
+++ headoftheloop.java
@@ -0,0 +1,43 @@
+package DSA;
+class ListNode {
+    int val;
+     ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
+public class headoftheloop {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
+
+        }
+        return null;
+    }
+}
+
+/*
+HashMap<ListNode,Integer> mp=new HashMap<>();
+        ListNode temp=head;
+        while(temp!=null){
+            if(mp.containsKey(temp)){
+                return temp;
+            }
+            mp.put(temp,1);
+            temp=temp.next;
+        }
+        return null;
+ */
