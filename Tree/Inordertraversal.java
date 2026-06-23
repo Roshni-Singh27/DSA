403ccb8521c0eea84ffe63cf57ac0059a3639007
https://github.com/Roshni-Singh27 <roshnisingh.27072005@gmail.com>
2026-06-23 22:35:22 +0530

    Done Inorder traversal of tree.

---
+++ Tree/Inordertraversal.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+ class TreeNode {
+     int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+public class Inordertraversal {
+     public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result=new ArrayList<>();
+        inorder(root,result);
+        return result;
+    }
+    void inorder(TreeNode root,List<Integer> result){
+        if(root==null)
+        return;
+        inorder(root.left,result);
+        result.add(root.val);
+        inorder(root.right,result);
+    }
+}
