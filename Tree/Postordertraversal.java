ddd0084eba5185dd7927ae5e5caa0a8c0e7e6fc2
https://github.com/Roshni-Singh27 <roshnisingh.27072005@gmail.com>
2026-06-28 22:47:27 +0530

    Done Post order travesal of tree

---
+++ Tree/Postordertraversal.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+     TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class Postordertraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result=new ArrayList<>();
+        postorder(root,result);
+        return result;
+    }
+    void postorder(TreeNode root,List<Integer> result){
+        if(root==null)
+        return;
+        postorder(root.left,result);
+        postorder(root.right,result);
+        result.add(root.val);
+    }
+}
